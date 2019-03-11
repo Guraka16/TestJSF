6de3c570d9bd9a16dab4123f02b4e26cb15ae5ef
Hari <haribabu.g77@gmail.com>
2019-03-10 22:02:15 -0500

    First Commit

---
+++ src/org/hari/jsf/Novel.java
@@ -0,0 +1,106 @@
+package org.hari.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Novel {
+
+	private String name;
+	private String author;
+	private String editor;
+	private String edition;
+	private String price;
+	private String[] genre;
+	private String description;
+	private String releaseDate;
+	private String origin;
+	private String saleOutOfIndia;
+	
+	public Novel()
+	{
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getEditor() {
+		return editor;
+	}
+
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String[] getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String[] genre) {
+		this.genre = genre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getSaleOutOfIndia() {
+		return saleOutOfIndia;
+	}
+
+	public void setSaleOutOfIndia(String saleOutOfIndia) {
+		this.saleOutOfIndia = saleOutOfIndia;
+	}
+	
+
+	
+}
